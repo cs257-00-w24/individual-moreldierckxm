5f83ad47191814d4f7a34ef1ed63639772070c2a
MutantCacti <mutantcacti@gmail.com>
2024-02-29 22:02:43 -0600

    Updated SQL table

--- Data/createtable.sql
+++ Data/createtable.sql
@@ -3,7 +3,7 @@ CREATE TABLE covid_cases_worldwide (
     dateRep DATE,
     cases INTEGER,
     deaths INTEGER,
-    countriesAndTerritories VARCHAR(255),
+    geoId VARCHAR(2),
     popData2019 INTEGER,
     continentExp VARCHAR(255),
     cumulativeCasesPer100000 FLOAT
